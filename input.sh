db259da1fc18cabac053aaf2267f39ef205d1358
Andrea Bocci <andrea.bocci@cern.ch>
2021-12-09 08:53:54 +0100

    Update the input samples for CMSSW_12_2_0_pre2

--- input.sh
+++ input.sh
@@ -9,27 +9,27 @@ SAMPLES="TTBAR ZMUMU" # ZEE ZTT
 DATA_SAMPLES="L1ACCEPT"
 
 # TTbar sample, with 2021 "realistic" conditions and pileup
-TTBAR="/RelValTTbar_14TeV/CMSSW_11_3_0_pre5-PU_113X_mcRun3_2021_realistic_v7-v1/GEN-SIM-DIGI-RAW"
-TTBAR_CACHE_PATH="/store/relval/CMSSW_11_3_0_pre5/RelValTTbar_14TeV/GEN-SIM-DIGI-RAW/PU_113X_mcRun3_2021_realistic_v7-v1/00000"
-TTBAR_CACHE_FILE="13fe6ebe-06f0-4932-80cd-90fa4493eba6.root,d3b63911-91f9-4167-b3c8-3010d63a376a.root"
+TTBAR="/RelValTTbar_14TeV/CMSSW_12_2_0_pre2-PU_122X_mcRun3_2021_realistic_v1-v3/GEN-SIM-DIGI-RAW"
+TTBAR_CACHE_PATH="/store/relval/CMSSW_12_2_0_pre2/RelValTTbar_14TeV/GEN-SIM-DIGI-RAW/PU_122X_mcRun3_2021_realistic_v1-v3/2580000"
+TTBAR_CACHE_FILE="29814f34-bdd3-4f68-89b7-c27754152787.root"
 TTBAR_NUMEVENTS=100
 
 # Z -> mumu sample, with 2021 "realistic" conditions, no pileup
-ZMUMU="/RelValZMM_14/CMSSW_11_3_0_pre5-113X_mcRun3_2021_realistic_v7-v1/GEN-SIM-DIGI-RAW"
-ZMUMU_CACHE_PATH="/store/relval/CMSSW_11_3_0_pre5/RelValZMM_14/GEN-SIM-DIGI-RAW/113X_mcRun3_2021_realistic_v7-v1/00000"
-ZMUMU_CACHE_FILE="9904ad4a-a5f6-473f-b035-2133fe5772d9.root,6352ccde-adc9-4004-a749-ecbb72207fbd.root"
+ZMUMU="/RelValZMM_14/CMSSW_12_2_0_pre2-122X_mcRun3_2021_realistic_v1-v1/GEN-SIM-DIGI-RAW"
+ZMUMU_CACHE_PATH="/store/relval/CMSSW_12_2_0_pre2/RelValZMM_14/GEN-SIM-DIGI-RAW/122X_mcRun3_2021_realistic_v1-v1/2580000"
+ZMUMU_CACHE_FILE="02369a2c-0164-46c1-85cf-94f4b811c7ad.root"
 ZMUMU_NUMEVENTS=200
 
 # Z -> ee sample, with 2021 "realistic" conditions, no pileup
-ZEE="/RelValZEE_14/CMSSW_11_3_0_pre5-113X_mcRun3_2021_realistic_v7-v1/GEN-SIM-DIGI-RAW"
-ZEE_CACHE_PATH="/store/relval/CMSSW_11_3_0_pre5/RelValZEE_14/GEN-SIM-DIGI-RAW/113X_mcRun3_2021_realistic_v7-v1/00000"
-ZEE_CACHE_FILE="4bea21c0-b9be-4e4e-8282-bb37ec7aa8c4.root"
+ZEE="/RelValZEE_14/CMSSW_12_2_0_pre2-122X_mcRun3_2021_realistic_v1-v1/GEN-SIM-DIGI-RAW"
+ZEE_CACHE_PATH="/store/relval/CMSSW_12_2_0_pre2/RelValZEE_14/GEN-SIM-DIGI-RAW/122X_mcRun3_2021_realistic_v1-v1/2580000"
+ZEE_CACHE_FILE="06e824c5-0606-453c-942e-da12fc529757.root"
 ZEE_NUMEVENTS=200
 
 # Z -> tautau sample, with 2021 "realistic" conditions, no pileup
-ZTT="/RelValZTT_14/CMSSW_11_3_0_pre5-113X_mcRun3_2021_realistic_v7-v1/GEN-SIM-DIGI-RAW"
-ZTT_CACHE_PATH="/store/relval/CMSSW_11_3_0_pre5/RelValZTT_14/GEN-SIM-DIGI-RAW/113X_mcRun3_2021_realistic_v7-v1/00000"
-ZTT_CACHE_FILE="a954d504-058b-45d2-a1e2-e22aa50d3aad.root"
+ZTT="/RelValZTT_14/CMSSW_12_2_0_pre2-122X_mcRun3_2021_realistic_v1-v1/GEN-SIM-DIGI-RAW"
+ZTT_CACHE_PATH="/store/relval/CMSSW_12_2_0_pre2/RelValZTT_14/GEN-SIM-DIGI-RAW/122X_mcRun3_2021_realistic_v1-v1/2580000"
+ZTT_CACHE_FILE="40ec2eb0-3f1e-4412-a20b-18a919b0b9cb.root"
 ZTT_NUMEVENTS=200
 
 # Level-1 Trigger selected events, from Run2018D data taking era, pileup 50
